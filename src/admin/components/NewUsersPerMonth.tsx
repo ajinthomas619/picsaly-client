71fca9f8f46556069711c586e3b522a171a756ce
ajin thomas <ajinthomas619@gmail.com>
2024-08-03 11:05:30 +0530

    may fix the prob

--- src/admin/components/NewUsersPerMonth.tsx
+++ src/admin/components/NewUsersPerMonth.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import CanvasJSReact from "@canvasjs/react-charts";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
